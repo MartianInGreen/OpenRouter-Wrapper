d1a513f55f7bccb822b51b13012e47400c04d340
MartianInGreen <github@rennersh.eu>
2024-09-27 08:38:46 +0200

    Add flake.nix for Python dev environment The changes introduce a new flake.nix file that sets up a Python development environment. It specifies inputs for nixpkgs and flake-utils, and defines outputs for default development shells using flake-utils. The file imports a shell.nix for package management.

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        shellFile = import ./shell.nix { inherit pkgs; };
+      in
+      {
+        devShells.default = shellFile;
+        #devShells.run = appFile;
+      }
+    );
+}
